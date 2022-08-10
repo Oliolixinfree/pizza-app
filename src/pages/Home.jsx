95e09e4062171522d4603296a1b445ce898a6975
Ivan <offlane@tut.by>
2022-08-10 21:42:41 +0300

    used react.context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,10 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
 import Categories from '../components/Categories';
 import Pagination from '../components/Pagination';
+import { SearchContext } from '../App';
 
-const Home = ({ searchValue }) => {
+const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);

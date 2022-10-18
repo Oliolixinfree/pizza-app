948a76b7fde0134a92dc2bf9e90272110c73ce87
Ivan <offlane@tut.by>
2022-10-18 20:33:54 +0300

    Publication in gh-pages. Installing gh-pages. Replacing BrowserRouter with HashRouter.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
@@ -10,10 +10,10 @@ if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
 
   root.render(
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
   );
 }
